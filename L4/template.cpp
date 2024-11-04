a0771effabf29d11a24c98d6cbe8c635c58b8d4a
CarlosVera81 <carlos.veraq@usm.cl>
2024-11-04 00:32:51 -0300

    update

---
+++ L4/template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+const ll mod = 1e9 + 7;
+const ll inf = 1e12;
+const ld pi = acos(-1);
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0);
+  cout << fixed << setprecision(9);
+
+
+  return 0;
+}
